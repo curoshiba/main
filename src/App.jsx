ec5be898a9aaf260cc236a3c89153938b6dadfba
curoshiba <101339491+curoshiba@users.noreply.github.com>
2022-03-16 13:52:04 +0900

    031603

--- src/App.jsx
+++ src/App.jsx
@@ -22,24 +22,40 @@ const App = () => {
         <h1>処理選択</h1>
         <div className="Icons">
           <div className="TopMenu">
-            <FaBook size={IconSize} />
-            <p>マニュアル</p>
-            <FaCheckSquare size={IconSize} />
-            <p>シフト提出</p>
-            <FaFileAlt size={IconSize} />
-            <p>シフト</p>
-            <FaList size={IconSize} />
-            <p>時間割登録</p>
+            <div className="ButtonIcon">
+              <FaBook size={IconSize} />
+              <div className="IconText">マニュアル</div>
+            </div>
+            <div className="ButtonIcon">
+              <FaCheckSquare size={IconSize} />
+              <div className="IconText">シフト提出</div>
+            </div>
+            <div className="ButtonIcon">
+              <FaFileAlt size={IconSize} />
+              <div className="IconText">アンケート</div>
+            </div>
+            <div className="ButtonIcon">
+              <FaList size={IconSize} />
+              <div className="IconText">時間割登録</div>
+            </div>
           </div>
-          <div UnderMenu>
-            <FaMoneyBillAlt size={IconSize} />
-            <p>給与一覧</p>
-            <FaInfoCircle size={IconSize} />
-            <p>注意事項</p>
-            <FaRegCalendarAlt size={IconSize} />
-            <p>シフト一覧</p>
-            <FaClock size={IconSize} />
-            <p>シフト提出</p>
+          <div className="UnderMenu">
+            <div className="ButtonIcon">
+              <FaMoneyBillAlt size={IconSize} />
+              <div className="IconText">給与一覧</div>
+            </div>
+            <div className="ButtonIcon">
+              <FaInfoCircle size={IconSize} />
+              <div className="IconText">注意事項</div>
+            </div>
+            <div className="ButtonIcon">
+              <FaRegCalendarAlt size={IconSize} />
+              <div className="IconText">シフト一覧</div>
+            </div>
+            <div className="ButtonIcon">
+              <FaClock size={IconSize} />
+              <div className="IconText">シフト提出</div>
+            </div>
           </div>
         </div>
       </div>
