9116694986a5622d052f8d5bb7e890aa0c9a133e
curoshiba <101339491+curoshiba@users.noreply.github.com>
2022-03-16 12:00:19 +0900

    031602

--- src/App.jsx
+++ src/App.jsx
@@ -13,23 +13,33 @@ import {
 import { Nav } from "./components/navbar";
 
 const App = () => {
+  const IconSize = 150;
   return (
     //メイン画面
+
     <>
       <div className="App">
         <h1>処理選択</h1>
         <div className="Icons">
           <div className="TopMenu">
-            <FaBook />
-            <FaCheckSquare />
-            <FaFileAlt />
-            <FaList />
+            <FaBook size={IconSize} />
+            <p>マニュアル</p>
+            <FaCheckSquare size={IconSize} />
+            <p>シフト提出</p>
+            <FaFileAlt size={IconSize} />
+            <p>シフト</p>
+            <FaList size={IconSize} />
+            <p>時間割登録</p>
           </div>
           <div UnderMenu>
-            <FaMoneyBillAlt />
-            <FaInfoCircle />
-            <FaRegCalendarAlt />
-            <FaClock />
+            <FaMoneyBillAlt size={IconSize} />
+            <p>給与一覧</p>
+            <FaInfoCircle size={IconSize} />
+            <p>注意事項</p>
+            <FaRegCalendarAlt size={IconSize} />
+            <p>シフト一覧</p>
+            <FaClock size={IconSize} />
+            <p>シフト提出</p>
           </div>
         </div>
       </div>
